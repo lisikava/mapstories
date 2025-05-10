09d928d82fc52b6e7ddf92c8ec7fb44b523b319b
Aliaksei Kanstantsinau <konstantinov.a.y2005@gmail.com>
2025-05-10 14:07:29 +0200

    Change jsonb to hstore, add indices and time fields

--- src/main/resources/scripts/schema.sql
+++ src/main/resources/scripts/schema.sql
@@ -2,5 +2,22 @@ create table if not exists pins (
     id bigint primary key generated always as identity,
     location point not null,
     category text not null check (category != ''),
-    tags jsonb not null check (tags is json object)
+    tags hstore not null,
+    create_time timestamptz not null default now(),
+    update_time timestamptz not null default now()
 );
+
+create index if not exists pins_location_gist on pins using gist(location);
+create index if not exists pins_tags_gin on pins using gin(tags);
+
+create or replace function set_update_time() returns trigger as $$
+begin
+    new.update_time := now();
+    return new;
+end;
+$$ language plpgsql;
+
+create or replace trigger set_update_time
+    before update on pins
+    for each row
+    execute function set_update_time();
