ca89044830e0090be012ef1b7b6ad06a89e79cff
Aliaksei Kanstantsinau <konstantinov.a.y2005@gmail.com>
2025-05-10 17:10:56 +0200

    Add docs and formatting

--- src/main/java/wat/Point.java
+++ src/main/java/wat/Point.java
@@ -1,3 +1,17 @@
 package wat;
 
-public record Point(double lat, double lon) {}
+/**
+ * Point on a map. The point is given by its geodetic coordinates with double
+ * resolution in WGS84 coordinate system.
+ *
+ * @param lat latitude
+ * @param lon longitude
+ */
+public record Point(double lat, double lon) {
+
+    @Override
+    public String toString() {
+        return String.format("(%1f, %2f)", lat, lon);
+    }
+
+}
