9b38d345fd80567e193bee0db96ef9c4d7d624e5
lisikava <mlisikava@gmail.com>
2025-05-11 16:09:11 +0200

    Javalin setup

--- src/main/java/wat/App.java
+++ src/main/java/wat/App.java
@@ -1,10 +1,17 @@
 package wat;
 
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/html", Location.CLASSPATH);
+        });
+        app.get("/", ctx -> { ctx.redirect("main.html"); });
+        app.start(7070);
     }
 }
