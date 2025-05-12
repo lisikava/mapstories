5084fab4600c9f4860b6359605d272d0e52b042e
huleha <gulegani@gmail.com>
2025-05-12 18:35:08 +0200

    Add method to retrieve pins

--- src/main/java/wat/Pin.java
+++ src/main/java/wat/Pin.java
@@ -1,15 +1,26 @@
 package wat;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Spliterators;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javax.sql.DataSource;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.postgresql.geometric.PGpoint;
+import org.postgresql.jdbc.PgArray;
 import org.postgresql.util.PGobject;
 
 public class Pin {
@@ -23,6 +34,25 @@ public class Pin {
         values (?, ?, ?)
         returning id, create_time""";
 
+    private static final String retrieveQuery =
+        """
+        with params as (select
+            ? as bbox,
+            ? as categories
+        )
+        select
+            pins.id,
+            pins.location,
+            pins.category,
+            hstore_to_json(pins.tags)::text as tags,
+            pins.create_time,
+            pins.update_time
+        from pins, params
+        where
+            (params.bbox is null or params.bbox @> pins.location) and
+            (params.categories is null or pins.category = any(params.categories));
+        """;
+
     public static Pin create(
         final Point location,
         final String category,
@@ -32,7 +62,7 @@ public class Pin {
         Timestamp createTime = null;
         try (Connection conn = dataSource.getConnection()) {
             PreparedStatement pstmt = conn.prepareStatement(createQuery);
-            pstmt.setObject(1, new PGpoint(location.toString()));
+            pstmt.setObject(1, location.asPGpoint());
             pstmt.setString(2, category);
             PGobject hstorePGobject = new PGobject();
             hstorePGobject.setType("hstore");
@@ -53,6 +83,56 @@ public class Pin {
             throw new RuntimeException(e);
         }
         return new Pin(id, location, category, tags, createTime, createTime);
+    }
+
+    public static List<Pin> retrieve(
+        BoundingBox bbox,
+        Collection<String> categories
+    ) {
+        List<Pin> foundPins = new ArrayList<>();
+        try (Connection conn = dataSource.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(retrieveQuery);
+            pstmt.setObject(1, bbox.asPGbox());
+            pstmt.setArray(2, conn.createArrayOf("text", categories.toArray()));
+            JSONParser parser = new JSONParser();
+            try (ResultSet resultSet = pstmt.executeQuery()) {
+                while (resultSet.next()) {
+                    Integer id = resultSet.getInt(1);
+                    Point location = new Point(
+                        resultSet.getObject(2, PGpoint.class)
+                    );
+                    String category = resultSet.getString(3);
+                    Map<String, String> tags = null;
+                    try {
+                        JSONObject obj = (JSONObject) parser.parse(
+                            resultSet.getString(4)
+                        );
+                        tags = new TreeMap<>();
+                        for (Object key : obj.keySet()) {
+                            tags.put((String) key, (String) obj.get(key));
+                        }
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Timestamp createTime = resultSet.getTimestamp(5);
+                    Timestamp updateTime = resultSet.getTimestamp(6);
+
+                    foundPins.add(
+                        new Pin(
+                            id,
+                            location,
+                            category,
+                            tags,
+                            createTime,
+                            updateTime
+                        )
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return foundPins;
     }
 
     private static String mapToHStore(Map<String, String> map) {
