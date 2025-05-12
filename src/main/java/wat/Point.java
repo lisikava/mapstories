b92c3355b056d1efe9280d1539145019815f5ffd
huleha <gulegani@gmail.com>
2025-05-12 18:31:17 +0200

    Add conversions to/from PGpoint

--- src/main/java/wat/Point.java
+++ src/main/java/wat/Point.java
@@ -1,5 +1,7 @@
 package wat;
 
+import org.postgresql.geometric.PGpoint;
+
 /**
  * Point on a map. The point is given by its geodetic coordinates with double
  * resolution in WGS84 coordinate system.
@@ -8,10 +10,16 @@ package wat;
  * @param lon longitude
  */
 public record Point(double lat, double lon) {
-
     @Override
     public String toString() {
         return String.format("(%1f, %2f)", lat, lon);
     }
 
+    public Point(PGpoint point) {
+        this(point.x, point.y);
+    }
+
+    public PGpoint asPGpoint() {
+        return new PGpoint(lat, lon);
+    }
 }
