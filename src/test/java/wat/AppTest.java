6cddd121238260f10fccc0b8143bb862265b911a
huleha <gulegani@gmail.com>
2025-05-12 21:38:08 +0200

    Add simple pin retrieval test

--- src/test/java/wat/AppTest.java
+++ src/test/java/wat/AppTest.java
@@ -1,7 +1,9 @@
 package wat;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.ArrayList;
+import java.util.TreeMap;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -9,11 +11,29 @@ import org.junit.jupiter.api.Test;
  */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
+    private void init() {
+        Pin.create(new Point(50, 50), "story", new TreeMap<>());
+        Pin.create(new Point(45, 45), "story", new TreeMap<>());
+    }
+
+    private void verbose(Pin pin) {
+        System.out.println("Id      : " + pin.getId());
+        System.out.println("Location: " + pin.getLocation());
+        System.out.println("Category: " + pin.getCategory());
+    }
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void pinRetrievalTest() {
+        init();
+        assertDoesNotThrow(() -> {
+            var categories = new ArrayList<String>();
+            categories.add("story");
+            var pins = Pin.retrieve(
+                new BoundingBox(60, 60, 40, 40),
+                categories
+            );
+            System.out.println("Found something, precisely: " + pins.size());
+            verbose(pins.get(0));
+        });
     }
 }
